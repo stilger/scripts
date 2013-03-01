e8273eebf7b1be60588663511d5f9f28ce46f29e
stilger <github@theic.org>
2013-02-28 22:05:50 -0500

    Modified updatedefault.sh to use git commands to create patch file

--- updatedefault.sh
+++ updatedefault.sh
@@ -1,15 +1,50 @@
-#!/bin/bash
-# I will add more logic and make the script nicer but for now
-# run this from your tmux directory
+#!/usr/bin/env bash
+
+#Set paths
+export NN_TMUX="/path/to/newznab/tmux"
+export BACKUP_PATH=$NN_TMUX"/backup"
+cd $NN_TMUX
 # create a backup directory first IE: mkdir backup
-#
-# create patch 
-diff -u config.sh.git config.sh > changes.patch
-# backup current defaults.sh
-cp -p defaults.sh backup/defaults.sh.`date +"%Y.%m.%d-%H.%M.%S"`
-# apply patch
+
+BackupDefaults()
+{
+echo "Backing up current defaults.sh"
+if [ -d "$BACKUP_PATH" ]; then
+cp -p $NN_TMUX/defaults.sh $BACKUP_PATH/defaults.sh.`date +"%Y.%m.%d-%H.%M.%S"`
+echo "backup of current defaults.sh complete"
+else
+echo "Backup did not run. Backup Directory does not exist"
+echo "Please create the backup directory"
+echo "Exiting."
+exit 0
+fi
+}
+
+GitFetch()
+{
+echo "Running git fetch -V"
+git fetch -v
+echo "git fetch complete"
+}
+
+CreatePatch()
+{
+echo "Running git diff against master to create patch file"
+git diff master origin/master config.sh > changes.patch
+echo "git diff complete"
+}
+
+ApplyPatch()
+{
+echo "Applying patch to defaults.sh"
 patch defaults.sh < changes.patch
-# backup old config.sh.git
-cp -p config.sh.git backup/config.sh.git.`date +"%Y.%m.%d-%H.%M.%S"`
-# create new config.sh.git
-cp -p config.sh config.sh.git
+echo "Patch applied"
+echo "Fix any hunks that did not apply correctly."
+echo "Now run git pull to update to current tmux version"
+}
+
+# Comment any that you do not want to run below. 
+BackupDefaults
+GitFetch
+CreatePatch
+ApplyPatch
