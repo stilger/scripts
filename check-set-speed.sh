b4470910b7eaf0170c133c313182cb611d902097
stilger <github@theic.org>
2020-01-06 08:47:00 -0500

    Added some documentation

--- check-set-speed.sh
+++ check-set-speed.sh
@@ -22,7 +22,12 @@ do
       echo "30 seconds left"
       sleep 30
       echo 'Setting speed'
-      screen -S miner0 -p 0 -X stuff "++++++++++1----------2----------3+++++++4++++++5++++++++++"
+      # For the SQRL Miner commands:
+      # a/#: select All or device # | +/-: Increase/decrease speed by 5 mhz | n/p: Select Next/Previous Device
+      # You will need to manually figure out the correct speeds for your devices then set speeds. I have mine commands
+      # increase all devices by 10 and then select my FK33's (1 and 2) and set them back to 800 by decreasing both by 10. 
+      # I then set the speed of each indiviudal device by number then by using the n to choose nest for each device past 9
+      screen -S miner0 -p 0 -X stuff "a++++++++++1----------2----------3+++++++4++++++5++++++++++"
       sleep 10
       screen -S miner0 -p 0 -X stuff "8+++9+n++++++n+++++++n+++++++n++++++++++n++++++++++n++++n++"
 
